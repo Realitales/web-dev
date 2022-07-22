9185a3eb54dab452c0dd8668b9a4db6747ae10f2
Jay Anthon Rico <jayanthon.rico@fligno.com>
2022-07-22 23:08:50 +0800

    added my finals to the repository

---
+++ about.js
@@ -0,0 +1,7 @@
+const navbarbtn = document.querySelector(".navbar-hamburger-btn");
+const navbar_container = document.querySelector(".navbar-container");
+
+navbarbtn.addEventListener("click", () => {
+  navbarbtn.classList.toggle("active-state");
+  navbar_container.classList.toggle("shown");
+});
